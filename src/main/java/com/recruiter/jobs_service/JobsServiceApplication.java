396da3c342e0a379e99561d64b86910dc2e7195b
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-06-27 11:14:41 +0530

    first commit

---
+++ src/main/java/com/recruiter/jobs_service/JobsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recruiter.jobs_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsServiceApplication.class, args);
+	}
+
+}
