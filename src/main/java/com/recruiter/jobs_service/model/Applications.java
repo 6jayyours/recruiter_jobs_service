0b74ae842ac622c0b4c3408b360219910c1f180e
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-07-09 14:37:49 +0530

    variable error

--- src/main/java/com/recruiter/jobs_service/model/Applications.java
+++ src/main/java/com/recruiter/jobs_service/model/Applications.java
@@ -21,5 +21,5 @@ public class Applications {
     private String resumeFileName;
     private String downloadUrl;
     private Integer postedId;
-    private Integer AppliedBy;
+    private Integer appliedBy;
 }
